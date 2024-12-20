54d0b3164f4193999470f65a8793dd402255d11e
DJ1211 <dan.jupe@yahoo.co.uk>
2024-12-20 14:00:29 +0000

    allow player to move around screen

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import pygame
 from constants import *
 from player import *
 
-
-def main():
-    print("Starting asteroids!")
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     
@@ -18,9 +17,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-       
+
+        for object in updatable:
+            object.update(dt)
+
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
+        for object in drawable:
+            object.draw(screen)
+
+
         pygame.display.flip()
 
         # limit fps to 60
