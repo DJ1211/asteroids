524fd888db645b1800a663eeb10582f851c6cbd2
DJ1211 <dan.jupe@yahoo.co.uk>
2024-12-24 10:53:21 +0000

    Added scoring & font required for score display

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import pygame.freetype
 import sys
-from constants import *
+from constants import * 
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -9,12 +10,14 @@ from shot import Shot
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    font = pygame.freetype.Font("AsteroidFont.ttf", 30)
     clock = pygame.time.Clock()
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
+    score = 0
     
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
@@ -36,15 +39,23 @@ def main():
         for object in asteroids:
             if object.check_collision(player):
                 print("Game over!")
+                print(f"Final Score: {score}")
                 sys.exit()
         
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.check_collision(shot):
                     asteroid.split()
+                    if asteroid.radius <= ASTEROID_MIN_RADIUS:
+                        score += 200
+                    elif asteroid.radius == ASTEROID_MAX_RADIUS:
+                        score += 100
+                    else:
+                        score += 150
                     shot.kill()
 
         screen.fill((0, 0, 0))
+        font.render_to(screen, (40, 40), f"{score}", (255, 255, 255))
         
         for object in drawable:
             object.draw(screen)
