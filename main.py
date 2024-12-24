ca3df800d2235c7801f698d57ac290182a569373
DJ1211 <dan.jupe@yahoo.co.uk>
2024-12-24 10:20:01 +0000

    Added shot and asteroid mechanics

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -13,9 +14,13 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
-    Asteroid.containers = (updatable, drawable, asteroids)
+    Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
 
@@ -32,24 +37,22 @@ def main():
             if object.check_collision(player):
                 print("Game over!")
                 sys.exit()
-
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    asteroid.split()
+                    shot.kill()
 
         screen.fill((0, 0, 0))
         
         for object in drawable:
             object.draw(screen)
 
-
         pygame.display.flip()
 
         # limit fps to 60
         dt = clock.tick(60) / 1000
-
-        
-        
-
-
-
 
 if __name__ == "__main__":
     main()
